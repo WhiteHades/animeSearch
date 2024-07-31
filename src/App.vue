5c2c1fb1d574ed266685cce4f07b3178295809be
Efaz Mohammed <44260523+WhiteHades@users.noreply.github.com>
2024-07-31 22:50:32 +0200

    added box, css

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
 <template>
   <div class="app">
     <header>
-      <h1><strong>TheAnimeDatabase</strong></h1>
+      <h1>The<strong>Anime</strong>Database</h1>
+      <form class="search-box">
+        <input
+          type="search"
+          class="search-field"
+          placeholder="Search for an anime..."
+        />
+      </form>
     </header>
   </div>
 </template>
@@ -14,5 +21,60 @@
   padding: 0;
   box-sizing: border-box;
   font-family: "Fira Sans", sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
+
+  h1 {
+    color: #888;
+    font-size: 42px;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    transition: 0.25s ease-in-out;
+    padding-bottom: 30px;
+
+    strong {
+      color: #313131;
+    }
+
+    &:hover {
+      color: #313131;
+    }
+  }
+
+  .search-box {
+    display: flex;
+    justify-content: center;
+    padding-right: 30px;
+    padding-left: 30px;
+
+    .search-field {
+      appearance: none;
+      background: none;
+      border: none;
+      outline: none;
+
+      background-color: #f3f3f3;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      padding: 15px;
+      border-radius: 8px;
+
+      color: #313131;
+      font-size: 20px;
+
+      transition: 0.25s;
+    }
+  }
 }
 </style>
