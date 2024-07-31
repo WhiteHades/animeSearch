057c87854eb6edb5bc1516b7daa863c99d5b3bb9
Efaz Mohammed <44260523+WhiteHades@users.noreply.github.com>
2024-07-31 23:58:34 +0200

    final code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
-<script setup lang="ts"></script>
+<script setup>
+import Card from "./components/Card.vue";
+import { ref } from "vue";
+
+const search_query = ref("");
+const animeList = ref([]);
+
+const handleSearch = async () => {
+  animeList.value = await fetch(
+    `https://api.jikan.moe/v4/anime?q=${search_query.value}`
+  )
+    .then((res) => res.json())
+    .then((json) => json.data);
+
+  console.log(animeList.value);
+};
+</script>
 
 <template>
   <div class="app">
     <header>
-      <h1>The<strong>Anime</strong>Database</h1>
-      <form class="search-box">
+      <h1><strong>Anime</strong> Search</h1>
+      <form class="search-box" @submit.prevent="handleSearch">
         <input
           type="search"
           class="search-field"
           placeholder="Search for an anime..."
+          required
+          v-model="search_query"
         />
       </form>
     </header>
+
+    <main>
+      <div class="cards">
+        <Card v-for="anime in animeList" :key="anime.mal_id" :anime="anime" />
+      </div>
+    </main>
   </div>
 </template>
 
@@ -34,7 +58,7 @@ header {
   h1 {
     color: #888;
     font-size: 42px;
-    font-weight: 400;
+    font-weight: 700;
     text-align: center;
     text-transform: uppercase;
     transition: 0.25s ease-in-out;
@@ -74,7 +98,31 @@ header {
       font-size: 20px;
 
       transition: 0.25s;
+
+      &::placeholder {
+        color: #aaa;
+      }
+
+      &:focus,
+      &:valid {
+        color: #fff;
+        background-color: #313131;
+        box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.15);
+      }
     }
+  }
+}
+
+main {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-left: 30px;
+  padding-right: 30px;
+
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -8px;
   }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card">
+    <a :href="anime.url" target="_blank">
+      <img :src="anime.images.webp.image_url" :alt="anime.title + ' Poster'" />
+      <h3>{{ anime.title }}</h3>
+    </a>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  anime: Object,
+});
+</script>
+
+<style lang="scss" scoped>
+.card {
+  flex: 1 1 33%;
+  width: 33%;
+  max-width: 33%;
+  padding: 0px 8px;
+  margin-bottom: 16px;
+
+  img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    border-radius: 16px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+    transition: 0.25s ease-in-out;
+  }
+
+  h3 {
+    padding: 16px;
+    color: #313131;
+  }
+
+  &:hover {
+    img {
+      transform: scale(1.05);
+    }
+
+    h3 {
+      color: #24c444;
+      transition: 0.25s ease-in-out;
+    }
+  }
+}
+</style>
